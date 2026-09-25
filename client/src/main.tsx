0d06f688e2f00b1bd5c4a75d4205a9da198fbb4c
Pratham Arora <166956829+Pratham-000@users.noreply.github.com>
2026-09-25 18:14:46 +0530

    feat: initialize theme provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,7 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
+import { ThemeProvider } from "./context/ThemeContext";
 import "./styles/reset.css";
 import "./styles/tokens.css";
 import "./styles/globals.css";
@@ -40,9 +41,11 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       }}
     >
       <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
+        <ThemeProvider>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </ThemeProvider>
       </QueryClientProvider>
     </Auth0Provider>
   </React.StrictMode>,
